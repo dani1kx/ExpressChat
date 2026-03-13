b008a12a969cd11ffe6b12fd3a24fea0cdaccc51
Nikita <habatynchik@gmail.com>
2026-03-13 18:05:09 +0200

    jjj

--- db/db-manager.js
+++ db/db-manager.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const {Pool} = require("pg");
 
 const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "postgres",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: "python",
     port: 5432,
     schema: "public"
